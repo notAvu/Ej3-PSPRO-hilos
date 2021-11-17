15907d13f6972789a3940eab68813d5da48e21d4
Álvaro Fernández <avunotfound@gmail.com>
2021-11-17 14:17:19 +0100

    Todo preparado para empezar a construir los hilos

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -14,19 +14,47 @@ public class Producto {
     private char tipo;//h=herramienta, r=recmbio, p=producto de limpieza
 
     public Producto(int cantidad, char tipo) {
-        setPrecioUnitario();
         this.cantidad = cantidad;
         this.tipo = tipo;
+        setPrecioUnitario();
     }
 
     public double getPrecioUnitario() {
         return precioUnitario;
     }
 
+    public void setPrecioUnitarioO() {
+        double maxPrecioTipo=0;
+        double minPrecioTipo=0;
+        switch(tipo) {
+            case 'h'-> {
+                maxPrecioTipo = MAX_HERRAMIENTAS_PRECIO;
+                minPrecioTipo = MIN_HERRAMIENTAS_PRECIO;
+            }
+            case 'r'->{
+                maxPrecioTipo = MAX_RECAMBIOS_PRECIO;
+                minPrecioTipo = MIN_RECAMBIOS_PRECIO;
+            }
+            case 'p'-> {
+                maxPrecioTipo = MAX_lIMPIEZA_PRECIO;
+                minPrecioTipo = MIN_lIMPIEZA_PRECIO;
+            }
+        }
+        this.precioUnitario = getPrecioRandom(maxPrecioTipo, minPrecioTipo);
+    }
     public void setPrecioUnitario() {
-        Random random= new Random();
-        double preciounitario= random.nextDouble();
+        double precioUnitario=0;
+        switch(tipo) {
+            case 'h'-> precioUnitario = getPrecioRandom(MAX_HERRAMIENTAS_PRECIO, MIN_HERRAMIENTAS_PRECIO);
+            case 'r'-> precioUnitario = getPrecioRandom(MAX_RECAMBIOS_PRECIO, MIN_RECAMBIOS_PRECIO);
+            case 'p'-> precioUnitario = getPrecioRandom(MAX_lIMPIEZA_PRECIO, MAX_lIMPIEZA_PRECIO);
+        }
         this.precioUnitario = precioUnitario;
+    }
+
+    private double getPrecioRandom(double min, double max) {
+        Random random= new Random();
+        return random.nextDouble(max-min)+min;
     }
 
     public int getCantidad() {
